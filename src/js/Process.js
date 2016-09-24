41c82751eb61e59bbf0b97b436e7b72cad410ea6
Ace <ace.ellorinco@gmail.com>
2016-09-25 00:56:23 +0800

    Add Process data structure

---
+++ src/js/Process.js
@@ -0,0 +1,35 @@
+(function(global){
+
+	if(!global.CPUscheduling){
+		global.CPUscheduling = {};
+	}
+
+	var NO_VALUE = -1;
+
+
+	/**
+	 *	Container object for scheduling-related data of a single process
+	 *
+	 *	Holds the two data categories:
+	 *		initial data - 
+	 *		log data - data collected after the process is ran and completed
+	 */
+	function Process(id, burstTime, arrivalTime){
+		// Initial data
+		this.id = id;
+		this.burstTime = burstTime;
+		this.arrivalTime = arrivalTime;
+
+		// Log data
+		this.endTime = NO_VALUE;
+		this.waitTime = NO_VALUE;
+		this.startTime = NO_VALUE;
+		this.responseTime = NO_VALUE;
+	}
+
+
+	Process.NO_VALUE = NO_VALUE;
+
+	global.CPUscheduling.Process = Process;
+
+})(window);
