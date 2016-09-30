d3da0f68f9dc718bed8039f2c68b86c4070b80b7
Ace <ace.ellorinco@gmail.com>
2016-09-30 23:01:50 +0800

    Fix spacing on text gantt chart
    Process History mark spacing is now dynamically adjusted based on the number of digits of time. It now correctly aligns on larger-valued time.

--- src/js/ui/TextDisplay.js
+++ src/js/ui/TextDisplay.js
@@ -14,83 +14,91 @@
 	TextDisplay.generateTextGanttChart = function(pids, logList){
 		// Typechecking maybe?
 
-		var processHistory = {},
+		var processHistories = {},
 			pHistoryWrittenFlags = {},
 			logsItr = logList.getIterator(),
-			log, i, running, waiting, id,
-			time = 0, 
-			hr = "", timeText = "  ", tempText, chartText;
+			log, i, running, waiting, id, 
+			time = 0, nextDigit = 10,
+			leftPad = " ", rightPad = " ", oddFlag = true,
+			hrUnit = "___", hr = "", timeText = "", chartText = "";
 
 
+		// Populate
 		for(i = 0; i < pids.length; i++){
-			processHistory[pids[i]] = "";
+			processHistories[pids[i]] = "";
 			pHistoryWrittenFlags[pids[i]] = false;
 		}
 
+
 		while(logsItr.hasNext()){
+			// For counting padding
+			if(time === nextDigit){
+				if(oddFlag){
+					leftPad += " ";
+				}else{
+					rightPad += " ";
+				}
+
+				hrUnit += "_";
+
+				oddFlag = !oddFlag;
+				nextDigit *= 10;
+			}
+
 			log = logsItr.getNext();
 
+			// Running process
 			running = log.running;
 			if(running){
 				id = running.id;
-				
-				if(processHistory[id] || processHistory[id] === ""){
-					processHistory[id] += " R ";
+
+				if(processHistories[id] || processHistories[id] === ""){
+					processHistories[id] += leftPad + "R" + rightPad;
 				}
 
 				pHistoryWrittenFlags[id] = true;
 			}
-			
 
+			// Waiting processes
 			waiting = log.waiting;
 			for(i = 0; i < waiting.length; i++){
 				id = waiting[i].id;
 
-				if(processHistory[id] || processHistory[id] === ""){
-					processHistory[id] += " W ";
+				if(processHistories[id] || processHistories[id] === ""){
+					processHistories[id] += leftPad + "W" + rightPad;
 				}
 
 				pHistoryWrittenFlags[id] = true;
 			}
 
-
+			// Else
 			for(i = 0; i < pids.length; i++){
 				id = pids[i];
 
 				if(!pHistoryWrittenFlags[id]){
-					processHistory[id] += " - ";
+					processHistories[id] += leftPad + "-" + rightPad;
 				}else{
 					pHistoryWrittenFlags[id] = false;
 				}
 			}
 
+			hr += hrUnit;
 
-			hr += "___";
-
-			tempText = " " + (time++) + " ";
-			timeText += tempText.substring(tempText.length - 3);
+			timeText += " " + (time++) + " ";
+			
 		}
+
 
 		// Build chart
 		//chartText = "  " + hr + "\n";
-		chartText = "";
 		for(i = 0; i < pids.length; i++){
-			chartText += pids[i] + " |" + processHistory[pids[i]] + "\n";
+			chartText += pids[i] + " |" + processHistories[pids[i]] + "\n";
 		}
-		chartText += "  |" + hr + "\n";
-		chartText += timeText;
+		chartText += "   " + hr + "\n";
+		chartText += "   " + timeText;
 
 		return chartText;
 	}
-
-
-	/*
-		
-
-
-
-
-	*/
 
 
 	global.CPUscheduling.TextDisplay = TextDisplay;
