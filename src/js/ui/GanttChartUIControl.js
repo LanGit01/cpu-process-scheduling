d10eda58f11af8b89fe007a3494d072b8c71cdb7
Ace <ace.ellorinco@gmail.com>
2016-10-26 10:11:44 +0800

    Add monitoring on how long a direction key is pressed
    Knowing how long a direction key is pressed is invaluable for handling view movement acceleration. Movement acceleration is important when travelling long distances, as it decreases the time to do so while keeping the precision of moving in small distances. Spending an amount of time moving in the same direction should trigger acceleration.

--- src/js/ui/GanttChartUIControl.js
+++ src/js/ui/GanttChartUIControl.js
@@ -6,11 +6,13 @@
 		RIGHT = 3,
 		DOWN = 4,
 
-		BASE_VELOCITY = 1,
-		ADD_VELOCITY = 2,
-		VELOCITY_MULTIPLIER = 1.1,
+		BASE_SPEED = 1,
+		ADD_SPEED = 2,
+		SPEED_MULTIPLIER = 1.1,
+		MAX_SPEED = 20,
+		ACCEL_TIME_TRESHOLD = 1000,
 
-		DEFAULT_FPS = 12;
+		DEFAULT_MOVE_UPDATE_DELAY = 100;
 
 
 	var KeyToDirection = {
@@ -21,31 +23,93 @@
 	};
 
 
-	function GanttChartUIControl(ganttChart, fps){
+	function GanttChartUIControl(ganttChart, moveUpdateDelay){
 		this._ganttChart = ganttChart;
-		this._movementBounds = ganttChart.getPositionBoundsRect();
+		// Movement
+		this._x;
+		this._y;
+
+		this._xMin;
+		this._yMin;
+		this._xMax;
+		this._yMax;
+
+		this._currentSpeed = BASE_SPEED;
+		this._addSpeed = ADD_SPEED;
 
 		// Timing
-		this._fps = fps || DEFAULT_FPS;
-		this._timePerFrame = Math.floor(1000 / this._fps);
+		this._moveUpdateDelay = moveUpdateDelay || DEFAULT_MOVE_UPDATE_DELAY;
 		this._lastTime = 0;
+
+		// Keypress and direction
+		this._pressedDirection = null;
+		this._timeKeyDown = 0;
+	}
+
+
+	function move(that, direction){
+		var now = Date.now(), speed = that._currentSpeed;
+
+		if(now < that._lastTime + that._timePerFrame){
+			return;
+		}
+
+		that._lastTime = now;
 	}
 
 
 
+
+
+	function onKeyDown(e){
+		// Should be bounded to a GanttChartUIControl instance
+		var direction = KeyToDirection[e.key];
+
+		if(direction){
+			if(direction !== this._pressedDirection){
+				console.log("new direction");
+				this._pressedDirection = direction;
+				this._timeKeyDown = Date.now();
+			}
+
+			console.log(Date.now() - this._timeKeyDown);
+
+			e.preventDefault();
+		}
+
+		/*
+		if(direction){
+			
+			e.preventDefault();
+		}else{
+			console.log("none");
+		}
+		*/
+	}
+
+
+	function onKeyup(e){
+		this._pressedDirection = null;
+	}
+
+
 	GanttChartUIControl.prototype.initialize = function(){
-		var mainCanvas = this._ganttChart.getCanvas();
+		var mainCanvas = this._ganttChart.getCanvas(),
+			bounds = this._ganttChart.getPositionBoundsRect(),
+			keyDownFunc, keyUpFunc;
+
+		// Bounds
+		this._xMin = bounds.x;
+		this._xMax = bounds.x + bounds.w;
+		this._yMin = bounds.y;
+		this._yMax = bounds.y + bounds.h;
 
 		// Make canvas selectable
 		mainCanvas.tabIndex = 0;
 
-		mainCanvas.addEventListener("keydown", function(e){
-			console.log(e.key);
-		});
+		mainCanvas.addEventListener("keydown", onKeyDown.bind(this));
 
-		mainCanvas.addEventListener("keyup", function(e){
-			console.log(e.key);
-		});
+		mainCanvas.addEventListener("keyup", onKeyup.bind(this));
 
 
 	}
