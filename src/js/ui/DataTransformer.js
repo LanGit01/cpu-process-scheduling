a3fdb745f08d4105837b265ceba0f6b638804007
Ace <ace.ellorinco@gmail.com>
2017-01-28 23:54:58 +0800

    Create `DataTransformer` object
    `DataTransformer` acts as a mediator between the data from `Record` and the GUI. This way, the display GUI is decoupled from the raw data.

---
+++ src/js/ui/DataTransformer.js
@@ -0,0 +1,81 @@
+(function(global){
+
+	var DataTransformer = {
+		transform: function(record){
+			var rowids, rowidPrefix, log, logItr, transformFunc, i,
+				waiting, newWaiting, newLogs = [];
+
+			var rowids, rowidPrefix,
+				log, logItr, i,
+				waiting, newWaiting = [], newLogs = [],
+				transformFunc;
+
+
+			if(record.isMultilevel()){
+				rowids = [];
+
+				for(i = 0; i < record.getNumLevels(); i++){
+					rowids[i] = i;
+				}
+
+				transformFunc = multilevelTransform;
+				rowidPrefix = "L";
+			}else{
+				rowids = record.getPIDs();
+				transformFunc = singlelevelTransform;
+				rowidPrefix = "P";
+			}
+
+			// Transform logs
+			logItr = record.getLogs().getIterator();
+			while(logItr.hasNext()){
+				log = logItr.getNext();
+
+				// transform each waiting
+				waiting = log.waiting;
+				for(i = 0; i < waiting.length; i++){
+					newWaiting[i] = transformFunc(waiting[i]);
+				}
+
+				newLogs[newLogs.length] = {
+					running: transformFunc(log.running),
+					waiting: newWaiting
+				}
+			}
+
+			return {
+				rowids: rowids,
+				logs: newLogs
+			}
+		}
+	}
+
+	
+
+	function singlelevelTransform(d){
+		if(!d){
+			return null;
+		}
+
+		return {
+			rowid: d.process.id,
+			pid: d.process.id
+		};
+	}
+
+
+	function multilevelTransform(d){
+		if(!d){
+			return null;
+		}
+
+		return {
+			rowid: d.level,
+			pid: d.process.id
+		};
+	}
+
+
+	global.CPUscheduling.DataTransformer = DataTransformer;
+
+})(this);
