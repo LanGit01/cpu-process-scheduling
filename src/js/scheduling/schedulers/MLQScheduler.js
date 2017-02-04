ccf6a6bbf13fba362982232b72a561e9a247adeb
Ace <ace.ellorinco@gmail.com>
2017-02-04 20:13:04 +0800

    Finish working version of MLQScheduler
    Added check for preemption on the running level's scheduler. This is because any change on the current running process, whether it is termination or preemption, should trigger reevaluation of running level. `MLQScheduler` has been tested on different combinations of pairs of simple schedulers.

--- src/js/scheduling/schedulers/MLQScheduler.js
+++ src/js/scheduling/schedulers/MLQScheduler.js
@@ -19,6 +19,8 @@
 		this._runningLevel = schedulers.length;
 		this._top = schedulers.length;
 		this._preemptive = preemptive;
+
+		this._lastRunning = null;	// for checking preemption on individual levels
 	}
 
 
@@ -82,23 +84,23 @@
 
 
 	MLQScheduler.prototype.getWaitingProcesses = function(){
-		var i, levelScheduler, runningProcess, processes, waitingArr = [],
-			hasWaitingProcess;
+		var currentLevel, levelScheduler, running, hasWaiting,
+			waitingArr = [];
 
-		for(i = this._top; i < this._levels.length; i++){
-			levelScheduler = this._levels[i];
-			runningProcess = levelScheduler.getRunningProcess();
-			hasWaitingProcesses = levelScheduler.hasWaitingProcesses();
-		
-			if(i !== this._runningLevel && (hasWaitingProcesses && (runningProcess && runningProcess.remainingTime > 0))){
+		for(currentLevel = this._top; currentLevel < this._levels.length; currentLevel++){
+			levelScheduler = this._levels[currentLevel];
+			running = levelScheduler.getRunningProcess();
+			hasWaiting = levelScheduler.hasWaitingProcesses();
+
+			if(currentLevel !== this._runningLevel && (running && running.remainingTime > 0)){
 				waitingArr[waitingArr.length] = {
-					level: i,
+					level: currentLevel,
 					process: levelScheduler.getProcesses()
 				};
 			}else
-			if(hasWaitingProcesses){
+			if(hasWaiting){
 				waitingArr[waitingArr.length] = {
-					level: i,
+					level: currentLevel,
 					process: levelScheduler.getWaitingProcesses()
 				};
 			}
@@ -109,28 +111,48 @@
 
 
 	MLQScheduler.prototype.step = function(){
-		var hasRunningLevel, process,
+		var shouldReevaluate, running, level,
+			hasUnemptyLevel,
 			numLevels = this._levels.length;
 
-		if(this._top < numLevels){
-			hasRunningLevel = false;
+		/*
+			Should reevaluate when:
+				1. a process terminates/is preempted and top < running level
+				2. a process terminates/is preempted and there is no waiting process in the same level
 
+			Reevaluation:
+				Assumptions:
+					- this._top always points to the highest unempty level
+
+				Algorithm:
+					1. From this._top to end of levels, do:
+						- step the scheduler at the current level
+						- if there is running, finish the loop and set top = current level
+					2. If there are no unempty levels, set both top and runningLevel to length of level array
+		 */
+		if(this._top < numLevels){
+			shouldReevaluate = (this._runningLevel === numLevels);
+
+			// Check for termination
 			if(this._runningLevel < numLevels){
 				level = this._levels[this._runningLevel];
-				process = level.getRunningProcess();
-				hasRunningLevel = (process && process.remainingTime > 0) || level.hasWaitingProcesses();
+				running = level.getRunningProcess();
+				shouldReevaluate = ((running && running.remainingTime === 0) || level.shouldPreempt()) && (this._top < this._runningLevel || !level.hasWaitingProcesses());
+
+				// Check for preemption
+				shouldReevaluate = shouldReevaluate || (this._preemptive && this._top < this._runningLevel);
 			}
 
-			if(!hasRunningLevel || (this._preemptive && this._top < this._runningLevel)){
+			if(shouldReevaluate){
 				this._runningLevel = this._top;
-				hasRunningLevel = false;
+				hasUnemptyLevel = false;
 
-				while(this._runningLevel < numLevels && !hasRunningLevel){
+				while(this._runningLevel < numLevels && !hasUnemptyLevel){
 					level = this._levels[this._runningLevel];
 					level.step();
 
-					if(level.hasRunningProcess() || level.hasWaitingProcesses()){
-						hasRunningLevel = true;
+					if(level.hasRunningProcess()){
+						hasUnemptyLevel = true;
 					}else{
 						this._runningLevel++;
 					}
@@ -142,8 +164,6 @@
 			}
 		}
 	}
-
-
 
 	global.CPUscheduling.MLQScheduler = MLQScheduler;
 
