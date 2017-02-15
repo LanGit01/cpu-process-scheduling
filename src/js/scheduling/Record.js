da45b21dfc6794aead989670dbd0b22a78145df9
Ace <ace.ellorinco@gmail.com>
2017-02-15 10:17:01 +0800

    Rework `Record`
    `Record` changed into a factory to distinguish normal records and multilevel records. Giving it a separate creating function makes the intention clear.

--- src/js/scheduling/Record.js
+++ src/js/scheduling/Record.js
@@ -1,134 +1,100 @@
-(function(global){
+/**
+ *
+ *	Multi Level:
+ *
+ *		entry: {
+ *			running: {level, process},
+ *			waiting: {
+ *				level,
+ *				processes: [process]
+ *			}
+ *			event: 
+ *		}
+ *
+ *	Single Level:
+ *
+ *		entry: {
+ *			running: process
+ *			waiting: [process]
+ *			event:
+ *		}
+ *
+ *	
+ *
+ *
+ */
+ (function(Core){
+ 	/**
+ 	 *	Required modules/classes
+ 	 *		ProcessScheduling.Utils.LinkedList
+ 	 */
 
-	var LinkedList = global.CPUscheduling.LinkedList,
-		selectionSort = global.selectionSort;
-
-
-	function Record(pids, levels){
-		this._pids = selectionSort(pids, compareNum);
-		this._logs = new LinkedList();
-		this._levels = levels || null;
-	}
-
-
-	Record.prototype.getPIDs = function(){
-		return this._pids;
-	}
-
-
-	Record.prototype.getNumLevels = function(){
-		return this._levels;
-	}
-
-
-	Record.prototype.isMultilevel = function(){
-		return (this._levels !== null)
-	}
-
-
-	Record.prototype.getLogs = function(){
-		return this._logs;
-	}
-
-
-	Record.prototype.log = function(running, waiting){
-		/**
-		 *	Single Level: {
-		 *		running: process,
-		 *		waiting: Array[process]
-		 *	}
-		 *
-		 *	Multi level: {
-		 *		running: process,
-		 *		waiting: processes: Array[process]
-		 *	}
-		 */
-		 if(this.isMultilevel()){
-		 	this._logs.insert(multilevelLog(running, waiting));
-		 }else{
-		 	this._logs.insert(singlelevelLog(running, waiting));
-		 }
-	}
+ 	function Logger(multilevel){
+ 		this._logs = [];
+ 		this.isMultilevel = multilevel;
+ 	}
 
 
+ 	Logger.prototype.log = function(runningProcess, waitingProcesses){
+ 		if(this.isMultilevel){
 
-	/*---------------------------------------------*\
+ 		}else{
+ 			this._logs[this._logs.length] = createSingleLevelLog(runningProcess, waitingProcesses);
+ 		}
+ 	}
+
+
+ 	Logger.prototype.getLogs = function(){
+ 		return this._logs;
+ 	}
+
+
+ 	/*==================================================*\
 					Private Functions
-	\*---------------------------------------------*/
+	\*==================================================*/
+	
+ 	function createSingleLevelLog(runningProcess, waitingProcesses){
+ 		var i, waiting = [];
 
-	function multilevelLog(runningProcess, waitingLevels){
-		var i, j, running, waiting = [], level, waitingProcesses;
+ 		for(i = 0; i < waitingProcesses.length; i++){
+ 			waiting[i] = createProcessLogData(waitingProcesses[i]);
+ 		}
 
-		if(runningProcess){
-			running = createProcessData(runningProcess.process, runningProcess.level);
-		}
-
-		console.log("-----------------------------");
-		console.log(waitingLevels);
-
-		for(i = 0; i < waitingLevels.length; i++){
-			level = waitingLevels[i].level
-			waitingProcesses = waitingLevels[i].process;
-
-			console.log(waitingProcesses);
-			for(j = 0; j < waitingProcesses.length; j++){
-				waiting[waiting.length] = createProcessData(waitingProcesses[j], level);
-			}
-		}
+ 		return {
+ 			running: createProcessLogData(runningProcess),
+ 			waiting: waiting
+ 		};
+ 	}
 
 
 
-		return {
-			running: running,
-			waiting: waiting
-		};
-	}
+ 	function createProcessLogData(process, level){
+ 		if(process === null){
+ 			return null;
+ 		}
+
+ 		var pData = {
+ 			id: process.id,
+ 			remainingTime: process.remainingTime
+ 		};
+
+ 		if(level){
+ 			pData.level = level;
+ 		}
+
+ 		return pData;
+ 	}
 
 
-	function singlelevelLog(runningProcess, waitingProcesses){
-		var i, waiting = [];
+ 	Core.Record = {
+ 		getInstance: function(){
+ 			return new Logger(false);
+ 		},
 
-		for(i = 0; i < waitingProcesses.length; i++){
-			waiting[waiting.length] = createProcessData(waitingProcesses[i]);
-		}
-
-		return {
-			running: createProcessData(runningProcess),
-			waiting: waiting
-		}
-	}
+ 		getMultiLevelInstance: function(){
+ 			return new Logger(true);
+ 		}
+ 	};
 
 
-	function createProcessData(p, level){
-		if(!p){
-			return null;
-		}
-
-		var pData = {
-			id: p.id,
-			remainingTime: p.remainingTime
-		}
-
-		if(level > -1){
-			pData.level = level;
-		}
-
-		return pData;
-	}
-
-
-	function compareNum(a, b){
-		if(a > b){
-			return 1;
-		}else
-		if(a < b){
-			return -1;
-		}else{
-			return 0;
-		}
-	}
-
-
-	global.CPUscheduling.Record = Record;
-
-})(this);
+ })(ProcessScheduling.Core);
