605e57f02b2f6cddae76ac11f475eebf530257a1
Ace <ace.ellorinco@gmail.com>
2017-03-21 08:08:20 +0800

    Make `MLQScheduler` inherit from `MultilevelScheduler`
    Most class methods of `MLQScheduler` are moved to `MultilevelScheduler`.
    Unit test is updated.

--- src/js/core/schedulers/MLQScheduler.js
+++ src/js/core/schedulers/MLQScheduler.js
@@ -5,16 +5,16 @@
 	 *		ProcessScheduling.Utils	
 	 */
 
-	var LinkedList = Utils.LinkedList;
+	var LinkedList = Utils.LinkedList,
+		MultilevelScheduler = Schedulers.MultilevelScheduler;
 
 
 	function MLQScheduler(preemptive, schedulers){
-		this._levels = schedulers;
-		this._preemptive = preemptive;
-
-		this._top = schedulers.length;
-		this._currentLevel = schedulers.length;
+		MultilevelScheduler.call(this, schedulers, preemptive);
 	}
+
+
+	MLQScheduler.subclass(MultilevelScheduler);
 
 
 	MLQScheduler.prototype.acceptProcess = function(process, level){
@@ -23,119 +23,10 @@
 		}
 
 		this._levels[level].acceptProcess(process);
-		if(level < this._top){
-			this._top = level;
+		if(level < this._topIndex){
+			this._topIndex = level;
 		}
 	}
-
-
-	MLQScheduler.prototype.hasRunning = function(){
-		return (this._currentLevel < this._levels.length && this._levels[this._currentLevel].hasRunning());
-	}
-
-
-	MLQScheduler.prototype.hasWaiting = function(){
-		var i, hasWaiting = false;
-
-		if(!this.hasProcess()){
-			return false;
-		}
-
-		for(i = this._top; i < this._levels.length && !hasWaiting; i++){
-			if(i === this._currentLevel){
-				hasWaiting = this._levels[i].hasWaiting();
-			}else{
-				hasWaiting = this._levels[i].hasProcess();
-			}
-		}
-
-		return hasWaiting;
-	}
-
-
-	MLQScheduler.prototype.getRunning = function(){
-		if(this._currentLevel < this._levels.length){
-			return this._levels[this._currentLevel].getRunning();
-		}
-
-		return null;
-	}
-
-
-	MLQScheduler.prototype.getWaiting = function(){
-		var waiting = [], waitingOnLevel,
-			level, i;
-
-		for(levelIndex = this._top; levelIndex < this._levels.length; levelIndex++){
-			if(levelIndex === this._currentLevel){
-				waitingOnLevel = this._levels[levelIndex].getWaiting();
-			}else{
-				waitingOnLevel = this._levels[levelIndex].getProcesses();
-			}
-
-			for(i = 0; i < waitingOnLevel.length; i++){
-				waiting[waiting.length] = waitingOnLevel[i];
-			}
-		}
-
-		return waiting;
-	}
-
-
-	MLQScheduler.prototype.hasProcess = function(){
-		return (this._top < this._levels.length);
-	}
-
-
-
-	MLQScheduler.prototype.runningTerminated = function(){
-		return (this.hasRunning() && this._levels[this._currentLevel].runningTerminated());
-	}
-
-
-	MLQScheduler.prototype.shouldPreempt = function(){
-		return (this._preemptive && this._top < this._currentLevel);
-	}
-
-
-	MLQScheduler.prototype.hasNewStartingProcess = function(){
-		var running = this.getRunning();
-
-		return !!(running && running.remainingTime + 1 === running.burstTime);
-	}
-
-
-	MLQScheduler.prototype.contains = function(id){
-
-	}
-
-
-	MLQScheduler.prototype.getProcessLevelMap = function(){
-		var map = Object.create(null),
-			levelIndex, i, processes;
-
-		for(levelIndex = this._top; levelIndex < this._levels.length; levelIndex++){
-			processes = this._levels[levelIndex].getProcesses();
-
-			for(i = 0; i < processes.length; i++){
-				map[processes[i].id] = levelIndex;
-			}
-		}
-
-		return map;
-	}
-
-
-	MLQScheduler.prototype._findTopLevel = function(){
-		for(var topLevel = this._top; topLevel < this._levels.length; topLevel++){
-			if(this._levels[topLevel].hasProcess()){
-				break;
-			}
-		}
-
-		return topLevel;
-	}
-
 
 
 	MLQScheduler.prototype.step = function(){
@@ -145,8 +36,8 @@
 			return;
 		}
 
-		top = this._top;
-		current = this._currentLevel;
+		top = this._topIndex;
+		current = this._currentIndex;
 
 		if(current === this._levels.length){
 			shouldReevaluate = true;
@@ -163,7 +54,7 @@
 		}
 
 		if(shouldReevaluate){
-			this._top = this._currentLevel = current = this._findTopLevel();
+			this._topIndex = this._currentIndex = current = this._findTopLevel();
 		}
 
 		if(current < this._levels.length){
