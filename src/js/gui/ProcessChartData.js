1397e1fedd49e7c526cd91616f7433dab246b78b
Ace <ace.ellorinco@gmail.com>
2017-09-05 23:16:12 +0800

    Add log data wrapper class
    Add wrapper class `ProcessChartData` to provide a nice interface
    for the Gantt Chart GUI.

---
+++ src/js/gui/ProcessChartData.js
@@ -0,0 +1,72 @@
+define(function(){
+
+	/**
+	 *	Wrapper class for log data.
+	 *
+	 *	Represents a collection of states at each timestep of a Scheduler's level. Each timestep-state(log)
+	 *	is an object with the structure:
+	 *
+	 *		log = {
+	 *			running: Process,
+	 *			waiting: Array<Process>
+	 *		} 
+	 *
+	 *	If a `level` argument is provided, it will ignore processes not on that level. The logs will show
+	 *	only the processes on the specified level.
+	 *
+	 *	Simple schedulers only has a single level, the `level` parameter MUST be omitted, otherwise there
+	 *	will be an error.
+	 */
+	function ProcessChartData(logs, level){
+		if(level || level === 0){
+			this._logs = logs.map(function(log){
+				return filterLog(log, level);
+			});
+		}else{
+			this._logs = logs.map(function(log){
+				return {
+					running: log.running,
+					waiting: log.waiting
+				}
+			});
+		}	
+	}
+
+
+	ProcessChartData.prototype = {
+		constructor: ProcessChartData,
+
+		getNumLogs: function(){
+			return this._logs.length;
+		},
+
+		getRunning: function(time){
+			if(time > -1 && time < this._logs.length){
+				return this._logs[time].running;
+			}
+
+			return null;
+		},
+
+		getWaiting: function(time){
+			if(time > -1 && time < this._logs.length){
+				return this._logs[time].waiting;
+			}
+
+			return null;
+		}
+	};
+
+
+	function filterLog(log, level){
+		return {
+			running: (log.running && log.processLevelMap[log.running.id] === level ? log.running : null),
+			waiting: log.waiting.filter(function(process){
+				return log.processLevelMap[process.id] === level;
+			})
+		};
+	}
+
+
+	return ProcessChartData;
+});
