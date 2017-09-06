c35ef4e09ead2f7535236e1b119ac80b0d6e8444
Ace <ace.ellorinco@gmail.com>
2017-09-07 00:24:00 +0800

    Implement dimension calculation
    Create a function that calculates the row, column, and grid cell
    dimensions. The layout can be built using the result of the
    calculation.

--- src/js/gui/GanttChart.js
+++ src/js/gui/GanttChart.js
@@ -23,17 +23,49 @@ define(function(){
 
 		// String Padding
 		c.stringVPad = ~~(c.fontSize * 0.2);
-		c.stringHPad = c.fontSize;
+		c.stringHPad = ~~(c.fontSize * 0.5);
 
 		// Label Dimensions
 		c.rowLabelWidth = options.rowLabelWidth;
-		//c.colLabelWidth = 40;
+	
+		// Cell Dimensions
+		c.cellWidth = options.cellWidth || options.cellHeight || 0;
+		c.cellHeight = options.cellHeight || options.cellWidth || 0;
 
 		// GUI dimensions
 		c.guiWidth = options.guiWidth || Defaults.GUI_WIDTH;
 		c.guiHeight = options.guiHeight || Defaults.GUI_HEIGHT;
 
 		return c;
+	}
+
+
+	function calculateDimensions(config, rowLabelWidth, colLabelWidth){
+		var gridWidth, gridHeight, rowLabelHeight;
+
+		rowLabelWidth += (2 * config.stringHPad);
+		colLabelWidth += (2 * config.stringHPad);
+
+		gridWidth = Math.max(config.cellWidth + (2 * Defaults.CELL_MARGIN), colLabelWidth);
+		gridHeight = Math.max(config.cellHeight + (2 * Defaults.CELL_MARGIN), config.fontSize + (2 * config.stringVPad));
+
+		if(config.cellWidth === 0 && config.cellHeight === 0){
+			gridWidth = gridHeight = rowLabelHeight = colLabelWidth = Math.max(gridWidth, gridHeight);
+		}else{
+			rowLabelHeight = gridHeight;
+		}
+
+		return{
+			gridWidth: gridWidth,
+			gridHeight: gridHeight,
+
+			rowLabelWidth: Math.max(rowLabelWidth, config.rowLabelWidth),
+			rowLabelHeight: rowLabelHeight,
+
+			colLabelWidth: colLabelWidth,
+			colLabelHeight: config.fontSize + (2 * config.stringVPad)
+		};
+		
 	}
 
 
