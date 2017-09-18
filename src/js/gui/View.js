49a3c1eb27d87326b6409a29f4884a64ae0651b9
Ace <ace.ellorinco@gmail.com>
2017-09-18 17:30:35 +0800

    Shield `setOffset` when no component available
    `setOffset` should now do nothing if the component is not set.

--- src/js/gui/View.js
+++ src/js/gui/View.js
@@ -13,8 +13,10 @@ define(function(){
 
 		setComponent: function(component){
 			this.component = component;
-			this._componentWidth = component.getWidth();
-			this._componentHeight = component.getHeight();
+			if(component){
+				this._componentWidth = component.getWidth();
+				this._componentHeight = component.getHeight();
+			}
 			return this;
 		},
 
@@ -31,18 +33,23 @@ define(function(){
 		},
 
 		setOffset: function(x, y){
+			if(!this.component) return this;
+
 			if(x < 0) x = 0;
 			if(y < 0) y = 0;
 
 			this._xOffset = Math.min(x, Math.max(0, this._componentWidth - this._w));
 			this._yOffset = Math.min(y, Math.max(0, this._componentHeight - this._h));
-			
+
 			return this;
 		},
 
 		draw: function(ctx){
-			ctx.strokeRect(this._x, this._y, this._w, this._h);
-			this.component.draw(ctx, this._x, this._y, this._w, this._h, this._xOffset, this._yOffset);
+			if(this.component){
+				this.component.draw(ctx, this._x, this._y, this._w, this._h, this._xOffset, this._yOffset);	
+			}
+
+			return this;
 		}
 	};
 
